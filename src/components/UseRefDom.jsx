0caa1b6d641a1b9419aa1e523714776cce46c4e3
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-05-19 01:04:09 +0530

    Add files via upload

---
+++ src/components/UseRefDom.jsx
@@ -0,0 +1,15 @@
+import React,{useRef} from 'react';
+function UseRefDom() {
+    const inputElement = useRef();
+    const focusInput = () => {
+        inputElement.current.focus();
+    }
+    return (
+        <>
+            <input type="text" ref={inputElement} />
+            <button onClick={focusInput}>Focus Input</button>
+        </>
+    );
+}
+
+export default UseRefDom;
