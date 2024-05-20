fceb7ce240c199af9ce3d0b703a656c699e73c46
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-05-21 00:29:40 +0530

    Updated UseReducerTodo.jsx

--- src/components/UseReducerTodo.jsx
+++ src/components/UseReducerTodo.jsx
@@ -4,7 +4,69 @@ const initialState = [
     {
         id: 1,
         title: "Todo 1",
-        check false,
+        check: false,
     },
-    
-]
+    {
+        id: 2,
+        title: "Todo 2",
+        check: false,
+    },
+    {
+        id: 3,
+        title: "Todo 3",
+        check: false,
+    },
+    {
+        id: 4,
+        title: "Todo 4",
+        check: false,
+    },
+    {
+        id: 5,
+        title: "Todo 5",
+        check: false,
+    },
+];
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "CHECK":
+            return state.map((item) => {
+                if (item.id === action.id) {
+                    return { ...item, check: !item.check }
+                }
+                else {
+                    return item;
+                }
+            });
+        default: return state;
+    }
+};
+
+const UseReducerTodo = () => {
+    const [list, dispatch] = useReducer(reducer, initialState);
+    const handleCheck = (item) => {
+        dispatch({ type: "CHECK", id: item.id });
+    };
+
+    return (
+        <>
+            {
+                list.map((item) => (
+                    <div key={item.id}>
+                        <label>
+                            <input 
+                                type="checkbox"
+                                checked={item.check}
+                                onChange={() => handleCheck(item)}
+                            />
+                            {item.title}
+                        </label>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default UseReducerTodo;
