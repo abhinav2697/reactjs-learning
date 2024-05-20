9466efd16f545a7eba6b533ae1fa5974708da637
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-05-21 00:01:37 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import UseStateHookObject from './components/UseStateHookObject';
 import UseEffectHook from './components/UseEffectHook';
 import UseEffectData from './components/UseEffectData';
 import UseContext from './components/UseContext';
+import UseRefHook from './components/UseRefHook';
+import UseRefDom from './components/UseRefDom';
+import UseRefState from './components/UseRefState';
+import UseReducerHook from './components/UseReducerHook';
+import UseReducerCounter from './components/UseReducerCounter';
+import UseReducerObject from "./components/UseReducerObject";
 function App() {
   return (
     <>
@@ -15,12 +21,25 @@ function App() {
       <UseEffectHook />
       <br /><br />
       <UseEffectData />
-      
-      <br />
-      <br />
-      <br />
+      <br /><br /><br />
       {"useContextHook"}
       <UseContext />
+      <br /><br /><br />
+      {"UseRefHook"}<br/>
+      <UseRefHook />
+      <br /><br />
+      {"UseRefHookDom"}<br/>
+      <UseRefDom />
+      {"UseRefStateHook"}<br />
+      <UseRefState />
+      {"UseReducerHook"}<br /><br />
+      <UseReducerHook />
+      {"UseReducerHookCounter"}<br /><br />
+      <UseReducerCounter />
+      {"UseReducerObject"}<br /><br />
+      <UseReducerObject/>
+      
+      
     </>
   );
 }
