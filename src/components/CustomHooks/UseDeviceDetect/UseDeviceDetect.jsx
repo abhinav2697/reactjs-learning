1f7fa6fe08eb7981c1960e536376581fcf7c0514
abhinav2697 <82115917+abhinav2697@users.noreply.github.com>
2024-05-29 21:15:49 +0530

    Delete src/components directory

--- src/components/CustomHooks/UseDeviceDetect/UseDeviceDetect.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const useDeviceDetect = () => {
-    const [isMobile, setMobile] = React.useState(false);
-    React.useEffect(() => {
-        const userAgent=typeof window.navigator===""
-    })
-}
